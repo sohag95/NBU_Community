f01400f1f092078157589251e20dd302ff6a71ee
sohag95 <roysohag95@gmail.com>
2022-10-28 23:26:50 +0530

    Navigated icon marked during visiting linked page.

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -8,3 +8,28 @@ if (document.querySelector("#signUp-form")) {
 if (document.querySelector(".header-search-icon")) {
   new Search()
 }
+
+if (document.querySelector("#student-home-page")) {
+  var element = document.getElementById("home-icon");
+  element.classList.add("navigate-mark");
+}
+if (document.querySelector("#student-notification-page")) {
+  var element = document.getElementById("notification-icon");
+  element.classList.add("navigate-mark");
+}
+if (document.querySelector("#student-batch-page")) {
+  var element = document.getElementById("batch-icon");
+  element.classList.add("navigate-mark");
+}
+if (document.querySelector("#student-department-page")) {
+  var element = document.getElementById("department-icon");
+  element.classList.add("navigate-mark");
+}
+if (document.querySelector("#student-group-page")) {
+  var element = document.getElementById("group-icon");
+  element.classList.add("navigate-mark");
+}
+if (document.querySelector("#student-profile-page")) {
+  var element = document.getElementById("profile-icon");
+  element.classList.add("navigate-mark");
+}
